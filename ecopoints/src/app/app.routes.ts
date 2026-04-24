f6ab974691291d47a0dbe238c94c9d14abc4865e
david_ <106620319+daxx-daxx@users.noreply.github.com>
2026-04-24 16:02:49 +0200

    Inicio

---
+++ ecopoints/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ScanComponent } from './pages/scan/scan';
+
+export const routes: Routes = [
+    { path: '', component: ScanComponent }
+];
---
+++ ecopoints/src/app/pages/scan/scan.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scan',
+  imports: [],
+  templateUrl: './scan.html',
+  styleUrl: './scan.scss',
+})
+export class ScanComponent {}
