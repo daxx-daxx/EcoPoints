1fa80d2a4062f002763e8c498587bda9dcc3ceab
davidcc03 <contrerascobodavid@gmail.com>
2026-05-06 20:59:03 +0200

    prueba conexion

---
+++ ecopoints/src/app/pages/prueba/prueba.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba',
+  imports: [],
+  templateUrl: './prueba.html',
+  styleUrl: './prueba.scss',
+})
+export class Prueba {}
